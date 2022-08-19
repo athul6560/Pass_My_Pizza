19a0022af2ded257f69dad5977c678029ee2ad5a
athulbabu <athul.6560@gmail.com>
2022-08-19 17:52:58 -0400

    order summary activity created

---
+++ app/src/main/java/com/zeezaglobal/passmypizza/Activities/ConfirmOrderActivity.java
@@ -0,0 +1,16 @@
+package com.zeezaglobal.passmypizza.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.zeezaglobal.passmypizza.R;
+
+public class ConfirmOrderActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_confirm_order);
+    }
+}
--- app/src/main/java/com/zeezaglobal/passmypizza/Activities/PizzaBuildPage.java
+++ app/src/main/java/com/zeezaglobal/passmypizza/Activities/PizzaBuildPage.java
@@ -3,17 +3,25 @@ package com.zeezaglobal.passmypizza.Activities;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.ImageView;
+import android.widget.Toast;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.zeezaglobal.passmypizza.R;
 
 public class PizzaBuildPage extends AppCompatActivity {
    private Button small, medium, large;
    private Button thinBtn, thickBtn;
     private ImageView thinCrust, thickCrust;
+    private ImageView tomatoImg, oliveImg,pestoImg,chieknImg,onionImg;
+    private CheckBox tomato,pesto,chiekn,olive,onions;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,9 +31,73 @@ public class PizzaBuildPage extends AppCompatActivity {
 
         onClicks();
 
+        checkBoxListner();
+
+    }
+
+    private void checkBoxListner() {
+
+        tomato.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                if(b)
+               tomatoImg.setVisibility(View.VISIBLE);
+                else
+                    tomatoImg.setVisibility(View.GONE);
+
+            }
+        });
+        pesto.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                if(b)
+                    pestoImg.setVisibility(View.VISIBLE);
+                else
+                    pestoImg.setVisibility(View.GONE);
+
+            }
+        });
+        olive.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                if(b)
+                    oliveImg.setVisibility(View.VISIBLE);
+                else
+                    oliveImg.setVisibility(View.GONE);
+
+            }
+        });
+        chiekn.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                if(b)
+                    chieknImg.setVisibility(View.VISIBLE);
+                else
+                    chieknImg.setVisibility(View.GONE);
+
+            }
+        });
+        onions.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                if(b)
+                    onionImg.setVisibility(View.VISIBLE);
+                else
+                    onionImg.setVisibility(View.GONE);
+
+            }
+        });
     }
 
     private void onClicks() {
+        findViewById(R.id.next_btn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+startActivity(new Intent(PizzaBuildPage.this,ConfirmOrderActivity.class));
+            }
+        });
+
+
         thinBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -87,5 +159,22 @@ public class PizzaBuildPage extends AppCompatActivity {
         thinBtn = findViewById(R.id.thin);
         thinCrust = findViewById(R.id.thin_img);
         thickCrust = findViewById(R.id.thick_img);
+
+
+
+        tomato = findViewById(R.id.tomato_sause_ch);
+        pesto = findViewById(R.id.pesto_ch);
+        chiekn = findViewById(R.id.chiekn_ch);
+        olive = findViewById(R.id.olive_ch);
+        onions = findViewById(R.id.onion_ch);
+
+
+
+        tomatoImg = findViewById(R.id.ketchup);
+        pestoImg = findViewById(R.id.pesto);
+        chieknImg = findViewById(R.id.chiken);
+        oliveImg = findViewById(R.id.olive);
+        onionImg = findViewById(R.id.onions);
+
     }
 }
